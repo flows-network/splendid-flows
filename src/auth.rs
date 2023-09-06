e951a997e18187fc79a9af7ccda73dd09bd097b0
wangshishuo <DarumaDocker@gmail.com>
2023-09-06 14:47:20 +0800

    debug

---
+++ src/auth.rs
@@ -0,0 +1,60 @@
+use base64::prelude::BASE64_STANDARD;
+use base64::write::EncoderWriter;
+use http_req::{
+    request::{Method, RequestBuilder},
+    tls,
+    uri::Uri,
+};
+use std::io::Write;
+use std::{convert::TryFrom, net::TcpStream};
+
+fn basic_auth<U>(username: U, password: U) -> String
+where
+    U: std::fmt::Display,
+{
+    let mut buf = b"Basic ".to_vec();
+    {
+        let mut encoder = EncoderWriter::new(&mut buf, &BASE64_STANDARD);
+        let _ = write!(encoder, "{}:{}", username, password);
+    }
+
+    String::from_utf8_lossy(&buf).into_owned()
+}
+
+pub(crate) async fn auth(code: String) {
+    let client_id = std::env::var("NOTION_CLIENT_ID").unwrap();
+    let client_secret = std::env::var("NOTION_CLIENT_SECRET").unwrap();
+    let redirect_uri = std::env::var("NOTION_REDIRECT_URI").unwrap();
+
+    let basic = basic_auth(client_id, client_secret);
+
+    let addr = Uri::try_from("https://api.notion.com/v1/oauth/token").unwrap();
+    let mut writer = Vec::new();
+    let stream = TcpStream::connect((addr.host().unwrap(), addr.corr_port())).unwrap();
+    let mut stream = tls::Config::default()
+        .connect(addr.host().unwrap_or(""), stream)
+        .unwrap();
+    let body = serde_json::to_vec(&serde_json::json!({
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirect_uri.as_str(),
+    }))
+    .unwrap();
+    match RequestBuilder::new(&addr)
+        .method(Method::POST)
+        .header("Connection", "Close")
+        .header("Authorization", basic.as_str())
+        .header("Content-Length", &body.len())
+        .body(&body)
+        .send(&mut stream, &mut writer)
+    {
+        Ok(response) => match response.status_code().is_success() {
+            true => {
+                let r = String::from_utf8_lossy(&writer).into_owned();
+                log::debug!("{}", r);
+            }
+            false => {}
+        },
+        Err(e) => {}
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+use serde_json::Value;
+use std::collections::HashMap;
+use webhook_flows::{create_endpoint, request_handler, send_response};
+
+mod auth;
+
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
-pub async fn on_deploy() {}
+pub async fn on_deploy() {
+    create_endpoint().await;
+}
+
+#[request_handler]
+async fn handle(
+    _headers: Vec<(String, String)>,
+    _subpath: String,
+    qry: HashMap<String, Value>,
+    _body: Vec<u8>,
+) {
+    let code = qry
+        .get("code")
+        .unwrap_or(&Value::Null)
+        .as_str()
+        .unwrap_or("");
+    auth::auth(code.to_string()).await;
+    send_response(200, vec![], vec![]);
+}
