8ae2dda1ad39a734fb2112bfcb6a6af5db1c31d8
DarumaDocker <DarumaDocker@gmail.com>
2023-09-14 11:01:15 +0800

    save url

--- src/lib.rs
+++ src/lib.rs
@@ -21,43 +21,64 @@ pub async fn on_deploy() {
 
 #[request_handler]
 async fn handler(
-    _qry: Vec<(String, String)>,
+    _headers: Vec<(String, String)>,
     _subpath: String,
-    _headers: HashMap<String, Value>,
+    qry: HashMap<String, Value>,
     _body: Vec<u8>,
 ) {
     logger::init();
-    let notion = NotionApi::new(std::env::var("NOTION_INTERNAL_SECRET").unwrap()).unwrap();
-    if let Ok(dbs) = notion.list_databases().await {
-        log::debug!("{:?}", dbs.results);
+    match qry.get("date") {
+        Some(date) if date.is_string() => {
+            let notion = NotionApi::new(std::env::var("NOTION_INTERNAL_SECRET").unwrap()).unwrap();
+            let page = new_page(date.as_str().unwrap());
+            _ = notion.create_page(page).await;
+            send_response(200, vec![], b"okok".to_vec());
+        }
+        _ => {
+            send_response(400, vec![], b"No date specified".to_vec());
+        }
     }
-    let page = new_page();
-    let r = notion.create_page(page).await;
-    log::debug!("{:?}", r);
-    send_response(200, vec![], b"okok".to_vec());
 }
 
-fn new_page() -> PageCreateRequest {
+fn new_page(date: &str) -> PageCreateRequest {
     let database_id = std::env::var("NOTION_PARENT_DATABASE_ID").unwrap();
     let database_id = DatabaseId::from_str(database_id.as_str()).unwrap();
 
     let title = RichText::Text {
         rich_text: RichTextCommon {
-            plain_text: String::from("New"),
+            plain_text: String::new(),
             href: None,
             annotations: None,
         },
         text: Text {
-            content: String::from("New Content"),
+            content: String::from(date),
             link: None,
         },
     };
-    let pv = PropertyValue::Title {
-        id: PropertyId::from_str("title").unwrap(),
+    let pd = PropertyValue::Title {
+        id: PropertyId::from_str("Date").unwrap(),
         title: vec![title],
     };
+
+    let phtml = PropertyValue::Url {
+        id: PropertyId::from_str("Html").unwrap(),
+        url: Some(format!(
+            "https://flows-access-analysis.s3.us-west-2.amazonaws.com/{}.html",
+            date
+        )),
+    };
+    let pcsv = PropertyValue::Url {
+        id: PropertyId::from_str("CSV").unwrap(),
+        url: Some(format!(
+            "https://flows-access-analysis.s3.us-west-2.amazonaws.com/{}.csv",
+            date
+        )),
+    };
+
     let mut properties = HashMap::new();
-    properties.insert(String::from("title"), pv);
+    properties.insert(String::from("Date"), pd);
+    properties.insert(String::from("Html"), phtml);
+    properties.insert(String::from("CSV"), pcsv);
     let properties = Properties { properties };
 
     PageCreateRequest {
