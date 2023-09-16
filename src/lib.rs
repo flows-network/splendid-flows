5541d6d730d864dee5c8103dc0dd557a04831fc3
DarumaDocker <DarumaDocker@gmail.com>
2023-09-16 16:41:00 +0800

    save messages to notion

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,26 @@ use discord_flows::{
         // application_command::CommandDataOptionValue,
         prelude::{
             application::interaction::application_command::ApplicationCommandInteraction, Channel,
+            Message,
         },
-        Message,
     },
     Bot, ProvidedBot,
 };
 use flowsnet_platform_sdk::logger;
 use serde_json::Value;
+use std::collections::HashMap;
+use std::str::FromStr;
+
+use notion_flows::notion::{
+    ids::{DatabaseId, PropertyId},
+    models::{
+        block::{CreateBlock, Text as BlockText, TextAndChildren},
+        properties::PropertyValue,
+        text::{RichText, RichTextCommon, Text, TextColor},
+        PageCreateRequest, Parent, Properties,
+    },
+    NotionApi,
+};
 
 use lazy_static::lazy_static;
 
@@ -81,15 +94,32 @@ async fn handler(ac: ApplicationCommandInteraction) {
                     .await
                 {
                     Ok(messages) if messages.len() > 0 => {
-                        log::debug!("{:?}", messages);
-                        _ = client
-                            .edit_original_interaction_response(
-                                &ac.token,
-                                &serde_json::json!({
-                                    "content": "Messages has been saved to Notion"
-                                }),
-                            )
-                            .await;
+                        let notion =
+                            NotionApi::new(std::env::var("NOTION_INTERNAL_SECRET").unwrap())
+                                .unwrap();
+                        let page = new_page(gc.name.as_str(), messages);
+                        match notion.create_page(page).await {
+                            Ok(_) => {
+                                _ = client
+                                    .edit_original_interaction_response(
+                                        &ac.token,
+                                        &serde_json::json!({
+                                            "content": "Messages has been saved to Notion"
+                                        }),
+                                    )
+                                    .await;
+                            }
+                            Err(_) => {
+                                _ = client
+                                    .edit_original_interaction_response(
+                                        &ac.token,
+                                        &serde_json::json!({
+                                            "content": "Failed save messages to Notion"
+                                        }),
+                                    )
+                                    .await;
+                            }
+                        }
                     }
                     _ => {
                         _ = client
@@ -133,5 +163,79 @@ async fn register_commands() {
     {
         Ok(_) => log::info!("Successfully registered command"),
         Err(err) => log::error!("Error registering command: {}", err),
+    }
+}
+
+fn new_page(thread_name: &str, messages: Vec<Message>) -> PageCreateRequest {
+    let database_id = std::env::var("NOTION_PARENT_DATABASE_ID").unwrap();
+    let database_id = DatabaseId::from_str(database_id.as_str()).unwrap();
+
+    let title = RichText::Text {
+        rich_text: RichTextCommon {
+            plain_text: String::new(),
+            href: None,
+            annotations: None,
+        },
+        text: Text {
+            content: String::from(thread_name),
+            link: None,
+        },
+    };
+    let pt = PropertyValue::Title {
+        id: PropertyId::from_str("Thread").unwrap(),
+        title: vec![title],
+    };
+
+    let mut properties = HashMap::new();
+    properties.insert(String::from("Thread"), pt);
+    let properties = Properties { properties };
+
+    let mut blocks = vec![];
+    for m in messages.into_iter() {
+        let header = RichText::Text {
+            rich_text: RichTextCommon {
+                plain_text: String::new(),
+                href: None,
+                annotations: None,
+            },
+            text: Text {
+                content: String::from(m.author.name),
+                link: None,
+            },
+        };
+        let header_block = CreateBlock::Heading3 {
+            heading_3: BlockText {
+                rich_text: vec![header],
+            },
+        };
+
+        blocks.push(header_block);
+
+        let para = RichText::Text {
+            rich_text: RichTextCommon {
+                plain_text: String::new(),
+                href: None,
+                annotations: None,
+            },
+            text: Text {
+                content: String::from(m.content),
+                link: None,
+            },
+        };
+        let para_block = CreateBlock::Paragraph {
+            paragraph: TextAndChildren {
+                rich_text: vec![para],
+                children: None,
+                color: TextColor::Default,
+            },
+        };
+
+        blocks.push(para_block);
+    }
+
+    PageCreateRequest {
+        parent: Parent::Database { database_id },
+        properties,
+        children: None,
     }
 }
