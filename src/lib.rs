0e96bb5654e66c4db60e57fea8b412b214cd3b5f
DarumaDocker <DarumaDocker@gmail.com>
2023-12-11 09:29:27 +0800

    Init discordish-project

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,49 @@
+use discord_flows::{
+    application_command_handler,
+    http::HttpBuilder,
+    message_handler,
+    model::{
+        prelude::application::interaction::application_command::ApplicationCommandInteraction,
+        Message,
+    },
+    Bot, ProvidedBot,
+};
+use flowsnet_platform_sdk::logger;
+
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
-pub async fn on_deploy() {}
+pub async fn on_deploy() {
+    let discord_token = std::env::var("DISCORD_TOKEN").unwrap();
+    let bot = ProvidedBot::new(&discord_token);
+
+    // register_commands().await;
+
+    bot.listen_to_messages().await;
+
+    bot.listen_to_application_commands().await;
+}
+
+#[message_handler]
+async fn handle(msg: Message) {
+    logger::init();
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+
+    let bot = ProvidedBot::new(token);
+    let client = bot.get_client();
+    let channel_id = msg.channel_id;
+    let content = msg.content;
+
+    if msg.author.bot {
+        log::debug!("message from bot");
+        return;
+    }
+
+    _ = client
+        .send_message(
+            channel_id.into(),
+            &serde_json::json!({
+                "content": content,
+            }),
+        )
+        .await;
+}
