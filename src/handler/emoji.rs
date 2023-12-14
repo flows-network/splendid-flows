89d1677041d4c63c4f950989785fde82e2471685
DarumaDocker <DarumaDocker@gmail.com>
2023-12-14 23:10:27 +0800

    Show all tasks

--- src/handler/emoji.rs
+++ src/handler/emoji.rs
@@ -28,6 +28,7 @@ pub async fn emoji(client: &Http, tc: &GuildChannel) -> &'static str {
             "roles": [guild_id]
         });
         let emoji = client.create_emoji(guild_id, &body, None).await.unwrap();
+        log::debug!("-----------------{}:{}", n.0, emoji.id.to_string());
         store_flows::set(n.0, Value::String(emoji.id.to_string()), None);
     }
 
